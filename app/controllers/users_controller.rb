ca4ce85820106fbcf552381aa34ea4290d73ef0f
Weyman Fung <wfung0083@yahoo.com>
2014-05-13 12:36:37 -0700

    sign in sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_url
+    else
+      render :new
+    end
+
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :sesion_token, :password)
+  end
+
+
 end
